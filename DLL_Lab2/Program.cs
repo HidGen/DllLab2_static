45758069f10ef32ade2a6828f095fa64c9c7b967
Spikehero <44679068+Spikehero@users.noreply.github.com>
2019-05-12 16:57:37 +0300

    Лаба с длл со статическим подключением

---
+++ DLL_Lab2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using FirstDll;
+
+namespace DLL_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+              Console.Write("Введите x: ");
+              double b = Convert.ToDouble(Console.ReadLine());
+
+              Console.Write("Введите y: ");
+              double c = Convert.ToDouble(Console.ReadLine());
+
+
+            double result = (Class1.Add(b, c));
+
+            Console.WriteLine(result);
+            result = (Class1.Sub(b, c));
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
+/*add и sub*/
