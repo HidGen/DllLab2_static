cc0b9c7746d6c158614cad892afdcd7253959a2a
Spikehero <44679068+Spikehero@users.noreply.github.com>
2019-05-17 12:19:35 +0300

    Работает как система

--- DLL_Lab2/Program.cs
+++ DLL_Lab2/Program.cs
@@ -12,23 +12,20 @@ namespace DLL_Lab2
     {
         static void Main(string[] args)
         {
-            
-
               Console.Write("Введите x: ");
               double b = Convert.ToDouble(Console.ReadLine());
-
               Console.Write("Введите y: ");
-              double c = Convert.ToDouble(Console.ReadLine());
-
-
-            double result = (Class1.Add(b, c));
-
-            Console.WriteLine(result);
-            result = (Class1.Sub(b, c));
-
+              double result, c = Convert.ToDouble(Console.ReadLine());
+            if ((b * c) >= 0)
+            {
+                 result = (Class1.Add(b, c));
+            }
+            else
+            {
+                 result = (Class1.Sub(b, c));
+            }
             Console.WriteLine(result);
             Console.ReadKey();
         }
     }
 }
-/*add и sub*/
